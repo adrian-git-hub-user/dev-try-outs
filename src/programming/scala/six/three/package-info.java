d6d34faf04eddb20be4edfebb3fc1a6e22b9f24c
adrian-git-hub-user <adrian.ronayne@gmail.com>
2013-07-21 23:13:29 +0100

    Update

---
+++ src/programming/scala/six/three/package-info.java
@@ -0,0 +1,15 @@
+
+/**
+ * This package contains code from the book "Programming Scala" by Dean Wampler; Alex Payne
+ * 
+ * 
+ * 
+ */
+/**
+ * code from 6.3 case classes
+ */
+/**
+ * 
+ *
+ */
+package programming.scala.six.three;
